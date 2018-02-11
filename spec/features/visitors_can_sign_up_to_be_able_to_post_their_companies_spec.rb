c260995ab489241877d7dea86696d395b8721f12
heybluguy <heybluguy@yahoo.com>
2018-02-10 17:58:33 -0700

    initial commit.

---
+++ spec/features/visitors_can_sign_up_to_be_able_to_post_their_companies_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'as a user i can see an option to sign up' do
+  describe 'when i am on the root path' do
+    scenario 'i can sign up with a username and password' do
+      visit '/'
+
+      click_on "Sign Up to Be a User"
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in "user[email]", with: "funbucket13"
+      fill_in "user[password]", with: "123"
+
+      click_on "Create User"
+
+      expect(page).to have_content("Welcome, funbucket")
+    end
+  end
+end
