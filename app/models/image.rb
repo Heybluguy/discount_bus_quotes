4179bd84f057ba69421d7bc426645fe19f1dece6
heybluguy <heybluguy@yahoo.com>
2018-02-15 02:38:58 -0700

    Fixed styling and images

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,9 @@ class Image < ApplicationRecord
   has_attached_file :image, styles: { small: "50x50>", thumb: "50x50>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-  def self.upload_image(company, file_path)
-    Image.create(image: File.new(file_path.tempfile.path, "r"), company: company)
+  def self.upload_image(company, images)
+    images.each do |image|
+      Image.create(image: File.new(image.tempfile.path, "r"), company: company)
+    end
   end
 end
